b7c8469f43512cab680a506a1250ce17242b9148
Michael <mikoa04@gmail.com>
2020-07-06 23:21:21 -0400

    Implementing useState function

--- mike_foods/src/App.js
+++ mike_foods/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Axios from 'axios';
 
 function App() {
+
+
   return (
     <div className="App">
       
