b8541dd55a88322d9387d99bc4b50c1c7b13ab20
Michael <mikoa04@gmail.com>
2020-07-08 11:25:16 -0400

    Creating the app on App.js

--- mike_foods/src/App.js
+++ mike_foods/src/App.js
@@ -7,7 +7,20 @@ function App() {
 	const [recipes, setRecipes] = useState([]);
 	const [alert, setAlert] = useState('');
 
-	return <div>Mike Foods</div>;
+	return (
+		<div className='App'>
+			<h1>Mike Food Search</h1>
+			<form className='search-form'>
+				<input
+					type='text'
+					name='query'
+					value=''
+					autoComplete='off'
+					placeholder='Search Food'
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default App;
